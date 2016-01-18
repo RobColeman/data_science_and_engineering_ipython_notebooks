57b2cf4162ba46ff00e2097e94c52599bf9bf05e
RobColeman <rob@preact.com>
2016-01-18 13:22:34 -0800

    tf-idf moved to it's own, LRU cache, loaders rm sklearn

--- metrics/distance_metrics.py
+++
@@ -1,1 +0,0 @@
-def cosine_distance():
--- util/loaders.py
+++ util/loaders.py
@@ -5,3 +5,9 @@ import sys
 def load_delimited(path,delimiter = ","):
     f = open(path,"r+")
     return [l.replace("\n","").split(delimiter) for l in f if not l == "\n"]
+
+def get_english_dictionary():
+    p = os.environ.get('DATA_HOME', None) + "/text/words/en"
+    f = open(p,"r+")
+    return [w.replace("\n","") for w in f.readlines() if not w == "\n"]
+
