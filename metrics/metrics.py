c5c0d6a3f908bbe274d577743e69c3d1f35b525e
RobColeman <rob@preact.com>
2016-02-10 13:54:01 -0800

    cross entropy

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -1,46 +1,57 @@
 import numpy as np
 
 def cosine_distance(A,B):
-  if type(A) == list:
-    A = np.array(A)
-  if type(B) == list:
-    B = np.array(B)
+    if type(A) == list:
+        A = np.array(A)
+    if type(B) == list:
+        B = np.array(B)
 
-  num = A.dot(B)
-  denom = np.linalg.norm(A) * np.linalg.norm(B)
+    num = A.dot(B)
+    denom = np.linalg.norm(A) * np.linalg.norm(B)
 
-  if denom == 0:
-    return 0.0
-  else:
-    return num / denom
+    if denom == 0:
+        return 0.0
+    else:
+        return num / denom
 
 
 def hamming_distance(A,B):
-  return sum([a==b for a,b in zip(A,B)])
+    return sum([a==b for a,b in zip(A,B)])
 
 
 def jaccard_index(A,B):
-  A = set(A)
-  B = set(B)
-  return float(len(A.intersection(B))) / float(len(A.union(B)))
+    A = set(A)
+    B = set(B)
+    return float(len(A.intersection(B))) / float(len(A.union(B)))
 
 
 def jaccard_distance(A,B):
-  return 1.0 - jaccard_index(A,B)
+    return 1.0 - jaccard_index(A,B)
 
 def tanimoto_distance(A,B):
-  """
-  for bit vector representation of sets
-  """
-  if type(A) == list:
-    A = np.array(A)
-  if type(B) == list:
-    B = np.array(B)
+    """
+    for bit vector representation of sets
+    """
+    if type(A) == list:
+        A = np.array(A)
+    if type(B) == list:
+        B = np.array(B)
 
-  num = float(A.dot(B))
-  denom = float(A.sum()**2 + B.sum()**2 - num)
+    num = float(A.dot(B))
+    denom = float(A.sum()**2 + B.sum()**2 - num)
 
-  if denom == 0:
-    return 0.0
-  else:
-    return num / denom
+    if denom == 0:
+        return 0.0
+    else:
+        return num / denom
+
+
+def cross_entropy_multinomial(true, test):
+    if not len(true) == len(test):
+        raise Exception('distributions must be of the same length')
+    total = 0
+    for p,q in zip(true,test)
+        total -= p * np.log(q)
+    return total
+
+
