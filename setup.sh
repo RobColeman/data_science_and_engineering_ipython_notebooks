86d632bec3ba782f5ff357f2ff2db54968768118
Robert Coleman <robert.coleman@chartboost.com>
2016-09-22 16:09:49 -0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo "export SPARK_HOME=/Users/rcoleman/spark" >> ~/.bash_profile
-echo "export DATA_HOME=/Users/rcoleman/rob/code/python/data_science_ipython_notebooks/data" >> ~/.bash_profile
-echo "export PROJECT_HOME=/Users/rcoleman/rob/code/python/data_science_ipython_notebooks" >> ~/.bash_profile
+echo "export DATA_HOME=/Users/robert.coleman/rob/data_science_and_engineering_ipython_notebooks/data" >> ~/.bash_profile
+echo "export PROJECT_HOME=/Users/robert.coleman/rob/data_science_and_engineering_ipython_notebooks" >> ~/.bash_profile
 
 export SPARK_HOME=/Users/rcoleman/spark
-export DATA_HOME=/Users/rcoleman/rob/code/python/data_science_ipython_notebooks/data
-export PROJECT_HOME=/Users/rcoleman/rob/code/python/data_science_ipython_notebooks
+export DATA_HOME=/Users/robert.coleman/rob/data_science_and_engineering_ipython_notebooks/data
+export PROJECT_HOME=/Users/robert.coleman/rob/data_science_and_engineering_ipython_notebooks
 # source setup.sh
