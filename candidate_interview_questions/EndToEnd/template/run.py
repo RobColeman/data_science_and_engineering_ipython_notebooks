794582cc384de59c27e0d5d0554d02c55e7fa8ef
Robert Coleman <rocolema@microsoft.com>
2018-03-23 12:44:26 -0700

    endtoendtutorial
    endtoendtutorial

---
+++ candidate_interview_questions/EndToEnd/template/run.py
@@ -0,0 +1,21 @@
+import json, os, sys
+
+
+"""
+solve the following
+- relative paths
+- reading files
+- url library
+-	examples ready
+- 
+
+"""
+
+def main(args):
+	""" Skeleton of python app"""
+	
+    return 0
+	
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
